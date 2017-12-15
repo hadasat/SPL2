c51197173031700ddfc7e097239975e3b11ae1e0
hadasat <atiyah@post.bgu.ac.il>
2017-12-15 17:49:29 +0200

    CoursePrivateState
    Promise

--- assignment2/src/main/java/bgu/spl/a2/sim/privateStates/CoursePrivateState.java
+++ assignment2/src/main/java/bgu/spl/a2/sim/privateStates/CoursePrivateState.java
@@ -1,5 +1,6 @@
 package bgu.spl.a2.sim.privateStates;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import bgu.spl.a2.PrivateState;
@@ -7,21 +8,24 @@ import bgu.spl.a2.PrivateState;
 /**
  * this class describe course's private state
  */
+
+
 public class CoursePrivateState extends PrivateState{
 
 	private Integer availableSpots;
 	private Integer registered;
 	private List<String> regStudents;
 	private List<String> prequisites;
-	
+
+
 	/**
  	 * Implementors note: you may not add other constructors to this class nor
 	 * you allowed to add any other parameter to this constructor - changing
 	 * this may cause automatic tests to fail..
 	 */
 	public CoursePrivateState() {
-		//TODO: replace method body with real implementation
-		throw new UnsupportedOperationException("Not Implemented Yet.");
+		regStudents = new ArrayList<>();
+		prequisites = new ArrayList<>();
 	}
 
 	public Integer getAvailableSpots() {
@@ -39,4 +43,10 @@ public class CoursePrivateState extends PrivateState{
 	public List<String> getPrequisites() {
 		return prequisites;
 	}
+
+	public void setAvailableSpots(Integer num){availableSpots = num;}
+
+	public void addRegister(){registered++;}
+
+	public void setRegistered(int num){registered = num;}
 }
