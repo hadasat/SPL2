e10ebc6f0af0cb3292b22ad594ea80b816d00d9f
Itay <itayshklar@gmail.com>
2017-12-16 00:51:12 +0200

    mnn

--- assignment2/src/main/java/bgu/spl/a2/actions/SubStudToCurs.java
+++ assignment2/src/main/java/bgu/spl/a2/actions/SubStudToCurs.java
@@ -2,6 +2,7 @@ package bgu.spl.a2.actions;
 
 import bgu.spl.a2.Action;
 import bgu.spl.a2.Promise;
+import bgu.spl.a2.sim.privateStates.CoursePrivateState;
 
 public class SubStudToCurs<Boolean> extends Action {
     private String student;
@@ -13,6 +14,7 @@ public class SubStudToCurs<Boolean> extends Action {
     @Override
     protected void start() {
         boolean isin = false;
-        
+        CoursePrivateState p = (CoursePrivateState)ActorPS;
+        isin = p.
     }
 }
