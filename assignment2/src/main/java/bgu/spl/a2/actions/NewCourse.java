1a266cc9661d7300ce35075a0756f89b75a389c3
Itay <itayshklar@gmail.com>
2017-12-15 22:50:07 +0200

    string

--- assignment2/src/main/java/bgu/spl/a2/actions/NewCourse.java
+++ assignment2/src/main/java/bgu/spl/a2/actions/NewCourse.java
@@ -14,7 +14,7 @@ public class NewCourse<Boolean> extends Action {
 
     @Override
     protected void start() {
-        Action<Boolean> sub1 = new SubCursToDep();
+        Action<Boolean> sub1 = new SubCursToDep(ActorID);
         subActions.add(sub1);
         sendMessage(sub1,dep, new DepartmentPrivateState());
         then(subActions, ()-> {complete(true);});
